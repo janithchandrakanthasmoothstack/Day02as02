eb5f54295fb4de05ee19c8a5176408024cb3806a
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2022-01-08 10:00:54 -0800

    calculate Fee method re developed using while loop

--- main.py
+++ main.py
@@ -6,16 +6,44 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import platform
 def main():
-  calculateFee()
-monthCount = 1
-def calculateFee(P=800000,R=6,M=10000,mc=1):
+  totalMonthCount = calculateFeeLoop()
+  print(f'Total Month Count : {totalMonthCount}')
+
+
+def calculateFeeLoop(P=800000,R=6,M=10000):
+    #init
+    newP = P
+    interestval = (P* (R/12))/100
+    monthCount = 0
+
+    print('Month        P      P  *  (R/12)%      -M          new P')
+
+    while newP>0:
+
+     monthCount = monthCount +1
+     interestval = (newP* (R/12))/100
+     xp = newP
+     if newP>M:
+      newP = newP-M+interestval
+      print(f'Paying {monthCount}         {xp}       ${interestval}        {M}       {newP} ')
+     else:
+      newP = newP+interestval
+      print(f'Paying {monthCount}         {xp}       ${interestval}        {newP}       0 ')
+      newP = 0
+    return monthCount
+
+def calculateFee(P=700000,R=6,M=10000,mc=1):
     interestval = (P* (R/12))/100
     newP = P-M+interestval
     print('Month        P      P  *  (R/12)%      -M          new P')
-    print(f'{mc}         {P}       ${interestval}        {M}       {newP} ')
-    if P>0 :
+    if (P>0) & (newP>0):
       mo = mc +1
-      calculateFee(newP,R,M,(mo))
+      if newP>M:
+        print(f'Paying {mc}         {P}       ${interestval}        {M}       {newP} ')
+        calculateFee(newP,R,M,(mo))
 
+      else:
+          print(f'Last Payment {mc}         {P}       ${interestval}        {M}       {newP} ')
+    return mo
 
 if __name__ == '__main__': main()
