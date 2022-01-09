feeeadb7d56a9b67c56b662b880d78105e697150
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2022-01-08 17:34:12 -0800

    Brootforce answer

--- main.py
+++ main.py
@@ -6,33 +6,35 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import platform
 def main():
-  totalMonthCount = calculateMonthlyFee()
-  print(f'Monthly Fee : {totalMonthCount}')
+  rental = calculateFeeLoop()
+  print('!!!!!!!!!!!!!!Final Answer !!!!!!!!!!!!!')
+  print(f'Monthly Rental >>> {rental:.2f}')
 
-def calculateMonthlyFee():
+  print('=======================================')
 
-    return 0
 
 def calculateFeeLoop(P=800000,R=6,M=10000):
     #init
     newP = P
     interestval = (P* (R/12))/100
     monthCount = 0
-
+    interest = 0
     print('Month        P      P  *  (R/12)%      -M          new P')
 
     while newP>0:
      monthCount = monthCount +1
      interestval = (newP* (R/12))/100
+     interest = interest + interestval
      xp = newP
      if newP>M:
       newP = newP-M+interestval
-      print(f'Paying {monthCount}         {xp}       ${interestval}        {M}       {newP} ')
+      print(f'Paying {monthCount}         {xp:.2f}       ${interestval:.2f}        {M}       {newP:.2f} ')
      else:
       newP = newP+interestval
-      print(f'Paying {monthCount}         {xp}       ${interestval}        {newP}       0 ')
+      print(f'Paying {monthCount}         {xp:.2f}       ${interestval:.2f}        {newP:.2f}       0 ')
       newP = 0
-    return monthCount
+    print(f'interest val : {interest:.2f}...total {(P+interest):.2f} ... monthly total: {((P+interest)/103):.2f} ')
+    return ((P+interest)/103)
 
 def calculateFee(P=700000,R=6,M=10000,mc=1):
     interestval = (P* (R/12))/100
