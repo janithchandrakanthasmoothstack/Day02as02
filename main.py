8153eb5946af9d691cea1792bd28d65f47900a5c
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2022-01-10 07:13:30 -0800

    improved with mathamatical function. complexcity f(1)

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 #!usr/bin/env python3
-
 # This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import platform
-def main():
-  rental = calculateFeeLoop()
-  print('!!!!!!!!!!!!!!Final Answer !!!!!!!!!!!!!')
-  print(f'Monthly Rental >>> {rental:.2f}')
 
+def main():
+  rental = monthlyCal()
+  print('!!!!!!!!!!!!!!Final Answer !!!!!!!!!!!!!')
+  print(f'Monthly Rental >>> {rental:.0f}')
   print('=======================================')
 
+def monthlyCal(P=800000,R=6,N=103):
+      #formula >   M = P [(1+r)^n]/[(1+r)^n - 1]]
+      #interest rate
+      r = (R/100)/12
+      print(f'interest : {r}')
+      # if k = (1+r)^n
+      K = pow((1+r),N)
+      print(f'k : {K}')
+      M = P *((r*K)/(K-1))
+      return M
 
 def calculateFeeLoop(P=800000,R=6,M=10000):
     #init
@@ -48,5 +56,4 @@ def calculateFee(P=700000,R=6,M=10000,mc=1):
       else:
           print(f'Last Payment {mc}         {P}       ${interestval}        {M}       {newP} ')
     return mo
-
 if __name__ == '__main__': main()
