99f0d8fb214f88cea7b08db66ca9f590f1ad7650
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2022-01-12 11:18:39 -0800

    word devided at the middle and its prefix and postfix checked

--- as_02/as_02_a/answer_05.py
+++ as_02/as_02_a/answer_05.py
@@ -1,4 +1,35 @@
 
 
+import math
+import re
 input = "Stats\nAmore, Roma\nNo 'x' in Nixon\nWas it a cat I saw?"
+
+input = "Stars\nO, a kak Uwakov lil vo kawu kakao!\nSome men interpret nine memos"
 data = input.split("\n")
+
+output = ""
+
+for x in data :
+  x =x.lower()
+  x = re.sub('[^a-z]+', '', x)
+  # x =x.replace("\'","")
+  # x =x.replace(",","")
+  # x =x.replace(" ","")
+  # x =x.replace("?","")
+  # x =x.replace("!","")
+
+  # print(math.floor(len(x)))
+  # print('..................')
+  # print(math.floor(len(x)/2))
+  # print('>>>>>>>>')
+  prefix = x[:math.floor(len(x)/2):]
+  postfix = x[math.floor(len(x)/2)+1:]
+  postfix = postfix[::-1]
+
+  if prefix == postfix:
+    # print(f"refix: {prefix} postfix: {postfix} answer: {prefix==postfix}")
+    output+='Y'
+  else: output+='N'
+
+
+  print(output)
