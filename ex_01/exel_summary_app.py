932bf2a1f756459fe9ecb51cca04af8558b90d51
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2022-01-13 18:21:04 -0800

    Read Excel component working, ready to develop search part

--- ex_01/exel_summary_app.py
+++ ex_01/exel_summary_app.py
@@ -1,8 +1,13 @@
 import sys
+from openpyxl import load_workbook
+
+baseDIR = ("E:\learn\problem_01")
+
 def fileNameValidator(fileName):
     fileParts = fileName.split("_");
     monthlist = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
                  'november', 'december']
+
     try:
         if(fileParts[0]!='expedia'):
             return "Incorrect File Name: File Name should start with the word expedia"
@@ -12,14 +17,37 @@ def fileNameValidator(fileName):
             return "Incorrect File Name: File third part should be the word 'monthly'"
         elif monthlist.count(fileParts[3])==0:
             return "Incorrect File Name: Fourth Part, Month name format error"
-        elif fileParts[4]>1900:
+        elif int(fileParts[4].split(".")[0])<1900:
             return "Incorrect File Name: Fifth part, incorrect Year Found"
-        else: return "ok"
-    except:return "Incorrect File Name Found !, format should be : expedia_report_monthly_january_2018.xlsx"
+        else: return [fileParts[3],int(fileParts[4].split(".")[0])]
+    except OSError as err:return "Incorrect File Name Found: {0}".format(err)
 
 def main():
     fileName = input("File Name Please: ")
     # fileName = 'expedia_report_monthly_january_20x8.xlsx'
     validateResponse = fileNameValidator(fileName)
-    print(validateResponse)
+
+    if isinstance(validateResponse,list):
+        #read file basic
+        print(validateResponse)
+        book = load_workbook(baseDIR+"\\"+fileName)
+        sheet = book.active
+        rows = sheet.rows
+        cols = sheet.columns
+        for x in rows:
+            print(x[0].value)
+        # headers = next(rows)
+
+        # headers = [cell.value for cell in next(rows)]
+        # print(headers)
+        # for row in rows:
+        #     data = {}
+        #     for title,cell in zip(headers,row):
+        #         data[title] = cell.value
+        #     print(data)
+
+        # sheet_2 = workbook.get_sheet(1)
+
+
+
 if __name__ == '__main__':main()
